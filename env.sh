88ae353078f2758eba905408db5833b00834e900
Rafael Hunt-Stokes <hunt-stokes@pplxint11.physics.ox.ac.uk>
2023-02-20 09:56:40 +0000

    updating env paths

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export IMAGING_ROOT="/path/to/imaging/directory"
-export DATA_ROOT="/path/to/data/directory"
-export ENV_FILE="/path/to/environment/file" # whatever you use for setting up RAT basically
-export RATROOT="/path/to/RAT"
+export IMAGING_ROOT="/home/hunt-stokes/imaging"
+export DATA_ROOT="/data/snoplus3/hunt-stokes/ILSReconPEP_data"
+export ENV_FILE="/home/hunt-stokes/rat4_env.sh" # whatever you use for setting up RAT basically
+export RATROOT="/home/hunt-stokes/rat4"
